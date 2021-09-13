091cae0233202baa533997f797dc0b65703b9d85
Aflahxrd <88531428+nexusNw@users.noreply.github.com>
2021-09-13 15:02:53 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,19 +11,19 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/YqVlUfk.jpg";
+  r_text[0] = "https://i.imgur.com/sme7UMb.jpeg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Ruby bot by nabuhandbz*\n
-\n*owner number wa.me/918289836232*\n
-\n*CONTACT OWNER THROUGH social networks-https://www.instagram.com/im.naabu?r=nametag*\n
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*FILO MADE BY AFLAHXRD 🙂💘*\n
+\n*owner number wa.me/918129624395*\n
+\n*CONTACT OWNER THROUGH social networks-instagram @nexus.efx*\n
 \n*bot setting video : https://youtu.be/VR_ppqEl2q8*\n
-\n*githublink       https://github.com/nabuhandbz/ruby*\n
-\n*audio commads     https://github.com/nabuhandbz/ruby/tree/master/uploads*\n
+\n*githublink       https://github.com/nexusNw/filo*\n
+\n*audio commads     https://github.com/nexusNw/filo/tree/master/uploads*\n
 `}) 
 
 }));
